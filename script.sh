77ac5fdfaf1fb73d52c9cfd2e66fb90427879d8a
strapmor <dimonmi0612@yandex.ru>
2024-10-28 14:23:58 +0700

    task 16

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Введите сообщение"read message
+echo "Введите идентификатор чата"
+read chat_id
+echo "Введите токен бота"read token
+curl "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message"
