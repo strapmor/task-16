dddd567985b777972371bff4a3d0addce5b0e898
strapmor <dimonmi0612@yandex.ru>
2024-10-29 18:38:42 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,6 @@ echo "Введите сообщение"
 read message
 echo "Введите идентификатор чата"
 read chat_id
-echo "Введите токен бота"read token
+echo "Введите токен бота"
+read token
 curl "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message"
